3d20f74f2d8c2575d6bc1da052fed617c5ec4111
Joeri Abbo <joeriabbo@hotmail.com>
2023-04-01 23:07:10 +0200

    Add more badges

--- get-badges.py
+++ get-badges.py
@@ -1,15 +1,21 @@
 import helper
+import multiprocessing
+
+
+def process_organization(organization):
+    print(organization['name'])
+
+    badges = helper.get_items_from_file(helper.get_badges_file(organization['id']))
+    if not len(badges) == 0:
+        print("Badges found skipping")
+        return
+
+    badges = helper.get_badges(badges, "https://www.credly.com" + organization['url'] + ".json")
+
+    helper.set_items_from_file(helper.get_badges_file(organization['id']), badges)
+
 
 if __name__ == '__main__':
     organizations = helper.get_items_from_file(helper.get_organizations_file())
-    for organization in organizations.values():
-        print(organization['name'])
-
-        badges = helper.get_items_from_file(helper.get_badges_file(organization['id']))
-        if not len(badges) == 0:
-            print("Badges found skipping")
-            continue
-
-        badges = helper.get_badges(badges, "https://www.credly.com" + organization['url'] + ".json")
-
-        helper.set_items_from_file(helper.get_badges_file(organization['id']), badges)
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+        pool.map(process_organization, organizations.values())
