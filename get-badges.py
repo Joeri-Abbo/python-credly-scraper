f26b7fd5c10d5c025e30d3f6724b5452e71694af
Joeri Abbo <joeriabbo@hotmail.com>
2023-04-01 22:15:10 +0200

    Add new badges.

--- get-badges.py
+++ get-badges.py
@@ -5,22 +5,32 @@ import helper
 
 def get_badges(badges, url):
     print(url)
-    r = requests.get(url).json()
-    for badge in r["data"]:
-        badges[badge['id']] = badge
-    if r["metadata"]["next_page_url"]:
-        get_badges(badges, r["metadata"]["next_page_url"])
+    try:
+        r = requests.get(url).json()
+        for badge in r["data"]:
+            badges[badge['id']] = badge
+        if r["metadata"]["next_page_url"]:
+            get_badges(badges, r["metadata"]["next_page_url"])
+    except:
+        print("Error")
     return badges
 
 
 if __name__ == '__main__':
+    skip = True
     organizations = helper.get_items_from_file(helper.get_organizations_file())
     for organization in organizations.values():
-        badges = helper.get_items_from_file(helper.get_badges_file())
-
         print(organization['name'])
-        # [None, 'Paid', 'Free']
+        if organization['name'] == "ERICSSON":
+            skip = False
 
-        badges = get_badges(badges, "https://www.credly.com/" + organization['url'] + ".json")
+        if not skip:
+            badges = helper.get_items_from_file(helper.get_badges_file())
 
-        helper.set_items_from_file(helper.get_badges_file(), badges)
+            # [None, 'Paid', 'Free']
+
+            badges = get_badges(badges, "https://www.credly.com/" + organization['url'] + ".json")
+
+            helper.set_items_from_file(helper.get_badges_file(), badges)
+        else:
+            print("skip")
