5df4e95fc15e67c751268bdcd2c6732c80708455
Joeri Abbo <joeriabbo@hotmail.com>
2023-04-01 23:15:00 +0200

    Refactor

--- index-badges.py
+++ index-badges.py
@@ -5,7 +5,10 @@ import helper
 if __name__ == '__main__':
     directory_path = "data/badges"
 
+    files = os.listdir(directory_path)
+    print(len(files))
     # Get all the files in the directory
-    helper.set_items_from_file(helper.get_badges_file("index"),
-                               os.listdir(directory_path)
-                               )
+    helper.set_items_from_file(
+        helper.get_badges_file("index"),
+        files
+    )
